55992b107b1dfb1184395c4cc0587408568a2dec
Javi Granados <jjavigrana2@gmail.com>
2025-04-15 19:28:04 +0200

    Diseño entrno gráfico acabado y validación de una expresión pasada por textBox acabada y funcional

--- CompiladorGrafic/CompiladorGrafic/MainWindow.xaml.cs
+++ CompiladorGrafic/CompiladorGrafic/MainWindow.xaml.cs
@@ -16,9 +16,39 @@ namespace CompiladorGrafic
     /// </summary>
     public partial class MainWindow : Window
     {
+        Compilador compilador = new Compilador();
+        List<object> datos = new List<object>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void t1_btn_validar_Click(object sender, RoutedEventArgs e)
+        {
+            string expresion = t1_tbox_expresion.Text;
+            string valido;
+            int resultado=0;
+
+            if (compilador.Validar(expresion))
+            {
+                valido = "Válido";
+                resultado = compilador.Resolver(expresion);
+            }
+            else
+                valido = "Inválido";
+            t1_tbox_expresion.Clear();
+
+            AgregarLineaDG(expresion, valido, resultado);
+        }
+        private void AgregarLineaDG(string expresion, string valido, int resultado)
+        {
+            datos.Add(new { Expresion = expresion, Validacion = valido, Resultado = resultado });
+            //Limpiar Data Grid
+            t1_dg_datos.ItemsSource = null;
+            //Poner datos en el Data Grid
+            t1_dg_datos.ItemsSource = datos;
+
         }
     }
 }
